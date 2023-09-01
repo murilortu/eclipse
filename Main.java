e93d82461e8c9247fb1c3d0d4592baacfc7f6223
murilo.rodrigues <116904598+murilortu@users.noreply.github.com>
2023-09-01 13:07:38 -0300

    Adicionei um arquivo

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int  x , y , c  ;
+		double area ;
+		
+		x = 5;
+		y = 2;
+		c = 1;
+		
+		area = (double) x / y ;
+		
+		System.out.printf("O valor de b %.3f" , area);
+	}
+
+}
---
+++ POO/src/br/edu/uft/poo/Cliente.java
@@ -0,0 +1,15 @@
+package br.edu.uft.poo;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		
+Cliente joao = new Cliente();
+		
+		joao.Cliente joao = new Cliente();
+		
+		joao.
+
+	}
+
+}
